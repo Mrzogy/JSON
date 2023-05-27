30d67bff62c90b83d53cca448d248acfc90a9fab
mrzogy <magbolmrzogy@gmail.com>
2023-05-27 15:52:28 +0300

    Initial Commit

---
+++ Jason/JasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  JasonApp.swift
+//  Jason
+//
+//  Created by 3bood on 07/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct JasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
